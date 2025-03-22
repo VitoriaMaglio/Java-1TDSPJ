616f6f70b550577786635596718b3e0b27b96e71
Vitória Valentina Maglio <vitoriamaglio@gmail.com>
2025-03-22 14:37:33 -0300

    Add files via upload

---
+++ Aula_teoria.java
@@ -0,0 +1,105 @@
+public class Aula_teoria {
+    public static void main(String[] args) {
+/* Orientação a objetos: POO (1960)
+POO é Programação orientada a objetos.
+Paradigma POO : tentar trazer o mundo real para o sistema Projeto Documenta&ccedil;&atilde;o Sistema
+Programação orientada a objetos tem o objetivo de orientar o c&oacute;digo em torno de objetos do mundo real, para entender as rela&ccedil;&otilde;es entre esses objetos para tentar tornar o c&oacute;digo mais pr&oacute;ximo do mundo real.
+
+Conceitos:
++ significa public
+Abstração : extrair o que mais importa de algo, ignorando detalhes desnecess&aacute;rios. Usada para entender s.c sob a perspectiva do orientador.
+Objetos dentro do JAVA :
+Classe = representa o obj do mundo real; modelo de um objeto, genérico, monta uma classe para criar objetos
+Diagrama de classe UML(Unified Modeling Language) representa visualmente os modelos do sistema de software e o DC ajuda a visualizar como as classes se integram.
+
+         Nome(negrito)
+         -var
+         +ações(o q cada obj faz dentro da classe)
+
+Obj é a representação concreta de uma classe.
+14/03
+Variável é um espaço na memória RAM
+Quando vc cria uma classe voce está criando um tipo novo
+Vc tbm tem que declarar o objeto com a palavra new ->
+tipo    nome      criar    tipo()
+Receita bolofuba = new Receita();
+métodos -> responsável por uma ação do objeto
++ exibirReceita (): VOID
+void significa que não tem retorno o método.
+método com retorno :
+String é um método que realiza a ação e retorna uma string, e deve ter um return
+
+20/03
+objeto é uma instância de memória, um novo tipo que contém características; é um conjunto de informações sobre algo do mundo real, Um objeto é uma instância de uma classe.
+variável: Uma variável é um espaço de memória que é utilizado para armazenar dados de diferentes tipos, como inteiros, strings, objetos, entre outros. Em Java, você precisa declarar o tipo da variável antes de usá-la.
+COMPOSIÇÃO: relação de objetos, assim dentro de um obj não existe mais variável e sim referências que se relacionam
+
+Classe descreeve o objeto;
+
+Enumeração é uma lista de valores para de um determinado atributo, para coisas finitas
+
+Quando on usúario tem opções, tem ESTRUTURA DE DECISÃO:
+Condição->
+Var/OperadorRelacional
+Resultado->
+Verdadeiro/Falso
+OperadorRelacional:
+> <
+>= <=
+== igual
+!= diferente
+
+Decisão simples: verifica o lado verdadeiro
+Decisão composta: verfifica os dois
+Decisão encadeada: testa condições, mais de duas decisões para avaliar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Tudo que você quer printar na tela voce precisa usar o comando leitor. pois você precisa que o sistemna leia o comando.
+leitor.nextInt();  para ler um número inteiro;
+leitor.nextLine();  parta ler nome composto
+
+
+
+
+
+ */
